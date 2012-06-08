fdd0ae182ee825a6a42f2dc58138697e03de7c06
Filipp Gelman <filipp@makerbot.com>
2012-06-08 14:03:40 -0400

    Implemented most of pathfinder interface

--- src/pathfinder.hpp
+++ src/pathfinder.hpp
@@ -9,14 +9,17 @@
 #define	PATHFINDER_HPP
 
 #include "topology.hpp"
+#include "sdl_app.hpp"
+
 #include <queue>
 #include <vector>
 #include <map>
+#include <list>
 
 namespace topo{
 
 template<typename UT = float, typename VT = vec2<UT> >
-class pathfinder{
+class pathfinder : public sdl_app{
 public:
 	
 	typedef typename topo_template<UT, VT>::unit_type unit_type;
@@ -25,8 +28,26 @@ public:
 	typedef typename topo_template<UT, VT>::link_type link_type;
 	typedef bool (*compare_func_type)(const unit_type& l, const unit_type& r);
 	
-	pathfinder(compare_func_type cf, node_type* sn, node_type* en);
+	enum STATUS{
+		STATUS_INIT,
+		STATUS_WORKING,
+		STATUS_FINISHED,
+		STATUS_FAILED
+	};
+	
+	pathfinder(compare_func_type cf, node_type* sn = NULL, node_type* en = NULL);
+	virtual void draw();
+	virtual STATUS get_status() const;
+	virtual bool done() const;
+	virtual node_type* get_start() const;
+	virtual node_type* get_end() const;
+	virtual void set_start(node_type* sn);	//also reinitializes state
+	virtual void set_end(node_type* en);	//also reinitializes state
+	virtual float get_render_radius() const;
+	virtual void set_render_radius(float rr);
 protected:
+		
+	virtual void reinit_state();
 	
 	class closed_type{
 	public:
@@ -53,12 +74,20 @@ protected:
 		compare_func_type compare_func;
 	};
 	
-	std::map<node_type*, closed_type> closed_set;
-	std::priority_queue<open_type, std::vector<open_type>, compare_wrapper> open_set;
+	typedef std::map<node_type*, closed_type> closed_set_type;
+	typedef std::priority_queue<open_type, std::vector<open_type>, compare_wrapper> open_set_type;
+	typedef std::list<link_type*> final_path_type;
+	
+	closed_set_type closed_set;
+	open_set_type open_set;
+	final_path_type final_path;
 	
 	node_type* start_node;
 	node_type* end_node;
 	
+	STATUS status;
+	
+	float render_radius;
 };
 
 template<typename UT, typename VT>
@@ -67,7 +96,83 @@ pathfinder<UT, VT>::pathfinder(
 		pathfinder<UT, VT>::node_type* sn, 
 		pathfinder<UT, VT>::node_type* en) : 
 		open_set(compare_wrapper(cf)), 
-		start_node(sn), end_node(en) {}
+		start_node(sn), end_node(en), 
+		status(STATUS_INIT), render_radius(0.05f){}
+template<typename UT, typename VT>
+void pathfinder<UT, VT>::draw(){
+	for(typename closed_set_type::iterator it = closed_set.begin(); 
+			it != closed_set.end(); 
+			++it){
+		glColor4f(0.f, 0.f, 1.f, 1.f);
+		typename closed_set_type::value_type cstvt = *it;
+		link_type* ltp = cstvt.second.dest_path;
+		vec2f pos = ltp->get_to()->get_position();
+		vec2f ppos = ltp->get_from()->get_position();
+		draw_circle(pos, get_render_radius());
+		glColor4f(0.f, 0.f, 1.f, 0.5f);
+		glBegin(GL_LINES);
+		glVertex2f(pos.x, pos.y);
+		glVertex2f(ppos.x, ppos.y);
+		glEnd();
+	}
+	if(done()){
+		glColor4f(1.f, 1.f, 0.f, 0.5f);
+		glBegin(GL_LINES);
+		for(typename final_path_type::iterator it = final_path.begin();
+				it != final_path.end();
+				++it){
+			typename final_path_type::value_type ltp = *it;
+			vec2f pos = ltp->get_to()->get_position();
+			vec2f ppos = ltp->get_from()->get_position();
+			glVertex2f(pos.x, pos.y);
+			glVertex2f(ppos.x, ppos.y);
+		}
+		glEnd();
+	}
+	glColor4f(1.f, 1.f, 1.f, 1.f);
+}
+template<typename UT, typename VT>
+typename pathfinder<UT, VT>::STATUS pathfinder<UT, VT>::get_status() const{
+	return status;
+}
+template<typename UT, typename VT>
+bool pathfinder<UT, VT>::done() const{
+	return get_status() == STATUS_FINISHED;
+}
+template<typename UT, typename VT>
+typename pathfinder<UT, VT>::node_type* pathfinder<UT, VT>::get_start() const{
+	return start_node;
+}
+template<typename UT, typename VT>
+typename pathfinder<UT, VT>::node_type* pathfinder<UT, VT>::get_end() const{
+	return end_node;
+}
+template<typename UT, typename VT>
+void pathfinder<UT, VT>::set_start(pathfinder<UT, VT>::node_type* sn){
+	start_node = sn;
+	reinit_state();
+}
+template<typename UT, typename VT>
+void pathfinder<UT, VT>::set_end(pathfinder<UT, VT>::node_type* en){
+	end_node = en;
+	reinit_state();
+}
+template<typename UT, typename VT>
+void pathfinder<UT, VT>::set_render_radius(float rr){
+	render_radius = rr;
+}
+template<typename UT, typename VT>
+float pathfinder<UT, VT>::get_render_radius() const{
+	return render_radius;
+}
+template<typename UT, typename VT>
+void pathfinder<UT, VT>::reinit_state(){
+	status = STATUS_INIT;
+	while(!open_set.empty())
+		open_set.pop();
+	closed_set.clear();
+	final_path.clear();
+}
 
 template<typename UT, typename VT>
 bool operator==(const typename pathfinder<UT, VT>::closed_type& lhs, 
